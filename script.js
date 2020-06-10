620306c06b7afc2c315ecee3ec3cdd519d4b4a91
Alexey Zhumaev <alexey221284@mail.ru>
2020-06-10 15:08:18 +0400

    fix: add owl carousel

---
+++ script.js
@@ -0,0 +1,25 @@
+$(function() {
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        margin: 1,
+        nav: false,
+        loop: true,
+
+
+        responsive: {
+            0: {
+                items: 1
+            },
+            480: {
+                items: 1
+            },
+            768: {
+                items: 1
+            },
+            1024: {
+                items: 1
+            }
+        }
+    });
+    
+   });
